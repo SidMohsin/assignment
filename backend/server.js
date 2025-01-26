ac6643178a4486792a0bd534f468f5ab1aad9660
SidMohsin <103144606+SidMohsin@users.noreply.github.com>
2025-01-26 21:24:43 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -55,5 +55,5 @@ app.post("/api/products", async (req, res) => {
   }
 });
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
